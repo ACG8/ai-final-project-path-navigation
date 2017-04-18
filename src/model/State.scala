d0ab43bbcd2a6e176c375a1304d27769c7ec4055
agiegerich <albert.a.giegerich@gmail.com>
2017-04-18 16:48:23 -0500

    Added comment

--- src/model/State.scala
+++ src/model/State.scala
@@ -1,6 +1,9 @@
 package model
 
 trait State {
+	// This signature won't work because successors has to handle ANY kind of state (whatever T is)
+	// a "PathState" can't return a list of "SudokuStates" as successors.
 	//def successors[T <: State](): List[T]
+
 	def successors(): List[State]
 }
