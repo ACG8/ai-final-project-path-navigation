4122e4b8c19837090d760c472e67c138f71db8aa
agiegerich <albert.a.giegerich@gmail.com>
2017-04-19 14:51:32 -0500

    Changed Float to Double

--- src/model/PathState.scala
+++ src/model/PathState.scala
@@ -4,7 +4,7 @@ package model
   * Created by agieg on 4/18/2017.
   */
 class PathState extends State {
-  def successors(): List[(State,Float)] = {
+  def successors(): List[(State,Double)] = {
     List()
   }
 }
