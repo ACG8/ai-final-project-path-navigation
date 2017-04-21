dd61dc4e96c74519ecf3307cadf86062a2928221
ACG <anandaguneratne@gmail.com>
2017-04-21 17:40:49 -0500

    Added new requirement to State trait
    States must know be able to determine if they are a goal state

--- src/model/State.scala
+++ src/model/State.scala
@@ -2,8 +2,13 @@ package model
 
 /**
   * Created by Ananda on 4/18/2017.
+  *
+  * Every state must know:
+  * 1. Which states are reachable and what their costs are relative to the current state
+  * 2. If they are a goal state or not
   */
 
 trait State {
 	def successors(): List[(State,Double)]
+	def isGoalState(): Boolean
 }
