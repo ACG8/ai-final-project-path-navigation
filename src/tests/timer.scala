217c3fec6ea8b780a4173f0ef14ea78afffd5d47
ACG <anandaguneratne@gmail.com>
2017-04-28 18:20:53 -0500

    Added a timer

---
+++ src/tests/timer.scala
@@ -0,0 +1,15 @@
+package tests
+
+/**
+  * Created by Ananda on 4/28/2017.
+  */
+object timer {
+  // This code was taken from http://biercoff.com/easily-measuring-code-execution-time-in-scala/
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block    // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0)/1000000 + "ms")
+    result
+  }
+}
