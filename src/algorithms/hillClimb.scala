57134e332d3dc2463f0caade63932f9874bcda7b
agiegerich <albert.a.giegerich@gmail.com>
2017-05-02 18:07:11 -0500

    -It's Richard -Changed tiebreaking behavior when two immediate successors have the same minimum heuristic of a k-successor -Added a timeout based on the number of vertices in the problem

--- src/algorithms/hillClimb.scala
+++ src/algorithms/hillClimb.scala
@@ -10,6 +10,8 @@ import model.State
   */
 
 object hillClimb {
+  val DELTA = 0.0001
+  // a record type to make things easier to read.
   case class StateBundle[T <: State[T]](state : T, kHeuristic : Double, relCost : Double )
 	//start is the starting state and h is the heuristic function
 
@@ -21,34 +23,52 @@ object hillClimb {
   }
 
   // a utility method
-  def minState[T <: State[T]]( x1 : StateBundle[T], x2 : StateBundle[T]) : StateBundle[T] = {
-    if (x1.kHeuristic < x2.kHeuristic) x1 else x2
-  }
 
   // next returns a StateBundle containing the immediate successor of n
   // with best (k - 1) descendant, and the relative cost of moving to that node
 
   def next[T <: State[T]](n: T, k : Int, h: T => Double): StateBundle[T] = {
-    val x: List[(T, Double)] = n.successors
-    x.map { case (m: T, relCost : Double) => StateBundle[T](m, bestKDescendant(m, k - 1, h), relCost) }
-      .reduceLeft {
-        minState[T]
-      }
-  }
-  // hillClimb returns the path,
-  def hillClimb[T <: State[T]](start: T, k: Int, h: T => Double): (List[T], Int, Double) = {
-	  def hillClimbHelper(h: T => Double, k: Int, path: List[T], numIter: Int, totalPathCost: Double): (List[T], Int, Double) = {
-      val nxt = next(path.head, k, h)
-      println("The current node's heuristic is " + h(path.head))
-      println("The candidate next node's k-descendant heuristic is " + nxt.kHeuristic);
-      nxt match {
-        case StateBundle(s, _, relCost) if s.isGoalState  => ((s :: path).reverse, numIter, totalPathCost + relCost)
-        case StateBundle(_, heur, _) if (heur >= h(path.head)) => (Nil, 0, 0.0)
-        case StateBundle(s, _, relCost) => hillClimbHelper(h, k, s :: path, numIter + 1, totalPathCost + relCost)
-      }
+    def minState( x1 : StateBundle[T], x2 : StateBundle[T]) : StateBundle[T] = {
+      if ( Math.abs(x1.kHeuristic - x2.kHeuristic) < DELTA)
+        if(h(x1.state) < h(x2.state)) x1 else x2
+      else
+      if(x1.kHeuristic < x2.kHeuristic) x1 else x2
     }
-    hillClimbHelper(h, k, List(start), 0, 0.0)
+    val x: List[(T, Double)] = n.successors
+    for (s <- x) {
+      if (s._1.isGoalState) return StateBundle(s._1, -1.0, s._2)
+    }
+    x.map { case (m: T, relCost: Double) => StateBundle[T](m, bestKDescendant(m, k - 1, h), relCost) }
+        .reduceLeft {
+          minState
+        }
+
   }
+  // hillClimb returns the path, the number of iterations used, and the total cost of the path.
+  // In case of failure, it returns (Nil, 0, 0.0)
+  def hillClimb[T <: State[T]](start: T, k: Int, h: T => Double): (List[T], Int, Double) = {
+	  def hillClimbHelper(h: T => Double, k: Int, path: List[T], numIter: Int, totalPathCost: Double, timeout: Int): (List[T], Int, Double) = {
+      if(timeout == 0) return (Nil, 0, 0.0)
+      var nxt = StateBundle(start,0,0.0)
+      try {
+        nxt = next(path.head, k, h)
+      } catch {
+        case _ : Throwable => return (Nil, 0, 0.0)
+      }
+        println("The current node is " + path.head.toString)
+        println("The candidate next node is " + nxt.state.toString)
+        println("The current node's heuristic is " + h(path.head))
+        println("The candidate next node's k-descendant heuristic is " + nxt.kHeuristic)
+        nxt match {
+          case StateBundle(s, _, relCost) if s.isGoalState  => ((s :: path).reverse, numIter, totalPathCost + relCost)
+          case StateBundle(_, heur, _) if (heur >= h(path.head)) => (Nil, 0, 0.0)
+          case StateBundle(s, _, relCost) => hillClimbHelper(h, k, s :: path, numIter + 1, totalPathCost + relCost, timeout - 1)
+        }
+      }
+
+    hillClimbHelper(h, k, List(start), 0, 0.0, start.size)
+  }
+  // kHillClimb, given an int k, gives the k-lookahead algorithm
   def kHillClimb[T <: State[T]](k : Int) : (T, T => Double) => (List[T], Int, Double)
     = (start : T, h : T => Double) => hillClimb(start, k, h)
 }
